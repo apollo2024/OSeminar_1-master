6159c62a5bad536108d90089ab3c3fb0505e41f9
muhammadkodyr <okk972016@gmail.com>
2023-04-03 00:31:10 +0500

    well done

--- src/main/java/h_m_1/Main.java
+++ src/main/java/h_m_1/Main.java
@@ -1,5 +1,7 @@
 package h_m_1;
 
+import java.util.ArrayList;
+
 /**
  * Создать наследника реализованного класса ГорячийНапиток с дополнительным полем int температура.
  * Создать класс ГорячихНапитковАвтомат реализующий интерфейс ТорговыйАвтомат и реализовать
@@ -10,6 +12,15 @@ package h_m_1;
  */
 public class Main {
     public static void main(String[] args) {
+        hotBeverangeVendingMachin machineHotWater = new hotBeverangeVendingMachin();
+        machineHotWater.addProduct(new hotBeverange("smth1", 450.0, 2, 60));
+        machineHotWater.addProduct(new hotBeverange("smth1", 897.0, 3, 30));
+        machineHotWater.addProduct(new hotBeverange("smth2", 300.0, 3, 50));
+        machineHotWater.addProduct(new hotBeverange("smth3", 300.0, 1, 40));
+        machineHotWater.addProduct(new hotBeverange("smth3", 200.0, 3, 50));
+        machineHotWater.addProduct(new hotBeverange("smth2", 200.0, 1, 50));
 
+        ArrayList<product> res = machineHotWater.getProduct("3", 1, 50);
+        System.out.println(res);
     }
 }
